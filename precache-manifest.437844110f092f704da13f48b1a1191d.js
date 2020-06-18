a4dea1b40b244e3178cf834c19d4849d00311044
Nama Rajeshwer <raj4raj003@gami.com>
2020-06-18 22:49:16 +0200

    Updates

---
+++ precache-manifest.437844110f092f704da13f48b1a1191d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c6f1f26b013d49e06a96711ba7278d8",
+    "url": "/DeadlyVirusCoronaTracker/index.html"
+  },
+  {
+    "revision": "2bff9994d0dd149d33b6",
+    "url": "/DeadlyVirusCoronaTracker/static/css/main.a59112b8.chunk.css"
+  },
+  {
+    "revision": "6ae7e0a86359b0eca3da",
+    "url": "/DeadlyVirusCoronaTracker/static/js/2.edbfd8ba.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/DeadlyVirusCoronaTracker/static/js/2.edbfd8ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bff9994d0dd149d33b6",
+    "url": "/DeadlyVirusCoronaTracker/static/js/main.af25cc6c.chunk.js"
+  },
+  {
+    "revision": "6b889d272c8ec7475219",
+    "url": "/DeadlyVirusCoronaTracker/static/js/runtime-main.799921dd.js"
+  },
+  {
+    "revision": "690051ed8f59df8991b74a5a97357882",
+    "url": "/DeadlyVirusCoronaTracker/static/media/image4.690051ed.png"
+  }
+]);
